02a28092c3da9fe4ca899f780a60c1b118a0f243
Ozgur KAYA <ozgur.kaya@trendyol.com>
2016-01-08 11:45:44 +0200

    files added

---
+++ Deploy.ps1
@@ -0,0 +1,10 @@
+﻿[string[]]$params = @(
+			"-setParam:name='IIS Web Application Name',value='" + $iisapplicationname + "'",
+			"-skip:Directory=^" + $iisapplicationname + "\\App_Data",
+			"-skip:File=.config$",
+
+			)
+$msdeployArgs = [string]::join(' ', $params)
+
+	.\obj\Release\Yourcsprojectname.Deploy.cmd /Y /M:localhost ($msdeployArgs) | Write-Output
+
